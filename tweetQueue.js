1feebc4a3f32fc9165eb6f098b10fb8068521991
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-07 15:51:40 -0700

    add archival

--- tweetQueue.js
+++ tweetQueue.js
@@ -15,12 +15,25 @@ export function processTweetQueue(oAuthAccessToken, getRequest) {
         try {
             const response = await getRequest(oAuthAccessToken, tweet);
             console.log('Tweet sent:', response);
+
+            // create archival object and archive the tweet
+            let tweetObject = ({ ...tweet, response, timestamp: new Date().toISOString() });
+            const archivePath = 'tweetArchive.json'
+            let archive = []
+            // check if the archive exists, if not then use the initialized archive
+            if (fs.existsSync(archivePath)) {
+                archive = JSON.parse(fs.readFileSync(archivePath, 'utf-8'))
+            }
+            archive.push(tweetObject)
+            fs.writeFileSync(archivePath, JSON.stringify(archive, null, 4))
+
             queue.shift()
-            fs.writeFileSync('queue.json', JSON.stringify(queue, null, 2))
+            fs.writeFileSync('queue.json', JSON.stringify(queue, null, 4))
         } catch (error) {
             console.error('Error sending tweet:', error);
+            reject(error)
         }
-        resolve(); // Resolve the promise after processing all tweets
+        resolve();
     });
 }
 
