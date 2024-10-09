d15d85b9210fe2dcb11fc71c8ac2326e9c0dead1
jjxrry <114961129+jjxrry@users.noreply.github.com>
2024-10-08 18:25:58 -0700

    add functions

--- removeFirst.js
+++ removeFirst.js
@@ -1,1 +1,22 @@
 // remove the first item in the queue (next one in line)
+import fs from 'fs'
+
+export function removeFirst() {
+    const queuePath = 'queue.json'
+    let queue = []
+
+    if (fs.existsSync(queuePath)) {
+        queue = JSON.parse(fs.readFileSync(queuePath, 'utf-8'))
+    }
+
+    let removedTweet = queue.shift()
+
+    if (removedTweet) {
+        console.log(removedTweet)
+    } else {
+        console.log("no tweet to remove")
+    }
+    fs.writeFileSync(queuePath, JSON.stringify(queue, null, 4))
+}
+
+removeFirst()
--- removeLast.js
+++ removeLast.js
@@ -1,1 +1,22 @@
 // remove the last item from the queue (most recent)
+import fs from 'fs'
+
+export function removeFirst() {
+    const queuePath = 'queue.json'
+    let queue = []
+
+    if (fs.existsSync(queuePath)) {
+        queue = JSON.parse(fs.readFileSync(queuePath, 'utf-8'))
+    }
+
+    let removedTweet = queue.pop()
+
+    if (removedTweet) {
+        console.log(removedTweet)
+    } else {
+        console.log("no tweet to remove")
+    }
+    fs.writeFileSync(queuePath, JSON.stringify(queue, null, 4))
+}
+
+removeFirst()
